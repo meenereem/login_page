57aca5a52f671fc2bf7baf117de7e60c5af23f02
Daniel Ng <dng@Locals-Mac-mini-2.local>
2018-04-10 14:57:13 -0500

    fixed

--- static/request.js
+++ static/request.js
@@ -1,8 +1,11 @@
 function addRequest() {
+    console.log("gotin")
     var newKeyPhrase = $("#new-request-KeyPhrase").val();
     var newTargetTerms = $("#new-request-TargetTerms").val();
-    var newsepKP = "False";
-    if ($('#"new-request-sepKP"').is(':checked')) {
+    // var newsepKP = $("#new-request-sepKP").val();
+    newsepKP = "False"
+    if ($("#new-request-sepKP").is(':checked')) {
+        console.log("wat")
         newsepKP = "True";
     }
     $.post(
