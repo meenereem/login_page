4141a1bd2c9db036cef4ef9ae009ee7909f1ac59
Daniel Ng <dng@Locals-Mac-mini-2.local>
2018-04-17 14:48:26 -0500

    use multiprocessing pool

--- analysis_daemon.py
+++ analysis_daemon.py
@@ -1,5 +1,6 @@
 import time, socket, MySQLdb, json
 from time import gmtime, strftime
+from multiprocessing import Pool
 from myrequests import *
 from postmarker.core import PostmarkClient
 
@@ -17,9 +18,21 @@ class Analyze:
         self.params = params
         self.result = result
 
+def process_request(info):
+    db = MySQLdb.connect("localhost","root","pass","morgdb")
+    cursor = db.cursor()
+    mylist = {"Key Phrase": info.KeyPhrase, "Target Terms": info.TargetTerms.replace('\n', ' ')[0: 100] + ("..."), "seperate Key Phrases": info.sepKP, "time": info.time, "complete_time": strftime("%Y-%m-%d %H:%M:%S", gmtime())}
+    done = "Done"
+    q_str = "INSERT INTO results ({0}, {1}, {2}) values (%s, %s, %s)".format(FIELD_ID, FIELD_PARAMS, FIELD_RESULT)
+    cursor.execute(q_str, [info.request_id, json.dumps(mylist), done])
+    del_row = "delete from requests where id = %s"
+    cursor.execute(del_row, [info.request_id])
+    db.commit()
+
 HOST = '127.0.0.1' 
 PORT = 3306
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pool = Pool(processes=4)
 try:
     sock.connect((HOST, PORT))
     print('connected')
@@ -35,13 +48,10 @@ while True:
             if info.in_progress == "False":
                 progress = "UPDATE requests SET in_progress='True' WHERE id = %s"
                 cursor.execute(progress, [info.request_id])
-                mylist = {"Key Phrase": info.KeyPhrase, "Target Terms": info.TargetTerms.replace('\n', ' ')[0: 100] + ("..."), "seperate Key Phrases": info.sepKP, "time": info.time, "complete_time": strftime("%Y-%m-%d %H:%M:%S", gmtime())}
-                done = "Done"
-                q_str = "INSERT INTO results ({0}, {1}, {2}) values (%s, %s, %s)".format(FIELD_ID, FIELD_PARAMS, FIELD_RESULT)
-                cursor.execute(q_str, [info.request_id, json.dumps(mylist), done])
-                del_row = "delete from requests where id = %s"
-                cursor.execute(del_row, [info.request_id])
                 db.commit()
+                print("1")
+                pool.apply_async(process_request, (info,))
+                print("2")
             # postmark = PostmarkClient(server_token='a27b1880-5284-4389-b274-b74d22b2b22c')
             # postmark.emails.send(
             # From='dng4@wisc.edu',
